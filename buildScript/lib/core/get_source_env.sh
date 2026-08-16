38dad0d02959716b54e03d8ba2de1fe90159b9ba
honglei <jhonglei@gmail.com>
2026-08-16 16:25:11 +0800

    build: update Neko core to 1.13.18-neko-1

--- buildScript/lib/core/get_source_env.sh
+++ buildScript/lib/core/get_source_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export REPOSITORY_SING_BOX="https://github.com/honglei/sing-box.git"
-export COMMIT_SING_BOX="7f0e9d1c7f3a2a13fdfa5fd81befe3fff0b345e7"
+export COMMIT_SING_BOX="1be1c8575127adf7bd19360b7973fae6cb8b2879"
 export REPOSITORY_LIBNEKO="https://github.com/MatsuriDayo/libneko.git"
 export COMMIT_LIBNEKO="1c47a3af71990a7b2192e03292b4d246c308ef0b"
 export REPOSITORY_GOMOBILE="https://github.com/MatsuriDayo/gomobile.git"
